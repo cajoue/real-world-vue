5c4c50c3c12ac04744e7d8036e7bdfab600ef62f
cajoue <cajoue@users.noreply.github.com>
2021-12-24 18:47:59 +0100

    03-outline-single-file-component

---
+++ src/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{ total }}</p>
+    <button @click="total += 1">Increment</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            total: 0
+        }
+    },
+}
+</script>
+
+<style>
+p {
+    font-size: 16px;
+}
+</style>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="event-card">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  // props: {
+  //   msg: String,
+  // },
+  data(){
+    return {
+      event: {
+        id: 245,
+        category: 'animal welfare',
+        title: 'Dog adoption day',
+        description : 'Find your new canine friend at this event',
+        location : 'Bark Island',
+        date : '28 January 2022',
+        time : '12:00',
+        petsAllowed : true,
+        organiser : 'Kaye Nine',
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
