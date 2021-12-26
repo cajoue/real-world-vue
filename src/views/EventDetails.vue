0323edce7e9784277c940c9a36c5906ca59592fd
cajoue <cajoue@users.noreply.github.com>
2021-12-26 16:27:00 +0100

    06 refactor with dynamic routing

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="event">
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
@@ -9,10 +9,10 @@
 <script>
 import EventService from "@/services/EventService.js"
 export default {
+  props: ["id"],
   data() {
     return {
       event: null,
-      id: 123,
     }
   },
   created() {
@@ -26,4 +26,9 @@ export default {
       })
   },
 }
+
+/* 
+a child component (EventDetails) with a prop (id) receives data 
+fed to it from a parent component (router-link) with params (id) 
+*/
 </script>
